49b579c385928f77b8d322ff62029333ab7e8c41
Gouthami Reddy <gouthamireddy@gouthamis-macbook-pro.local>
2018-06-18 11:33:06 -0400

    convert to MVC pattern

---
+++ TodoList/item.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  TodoList
+//
+//  Created by Gouthami Reddy on 6/18/18.
+//  Copyright © 2018 Gouthami Reddy. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
